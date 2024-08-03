312879351ad4a78e754deb7d9bb7c3289389f8ae
iamattri0001 <iamattri0001@gmail.com>
2024-08-03 12:55:10 +0530

    socket fix

--- client/src/contexts/SocketContext.jsx
+++ client/src/contexts/SocketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://webchat-mern.onrender.com", {
+      const socket = io("https://webchat-mern.onrender.com/", {
         query: {
           userId: authUser._id,
         },
